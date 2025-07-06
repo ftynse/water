e7909244c565642f4bb77ee072f26199c166f489
Ivan Butygin <ivan.butygin@gmail.com>
2025-07-06 16:10:05 +0200

    monolithic lvm build
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- build_tools/wheel/setup.py
+++ build_tools/wheel/setup.py
@@ -24,6 +24,10 @@ def invoke_cmake(*args, cwd=None):
     subprocess.check_call(["cmake", *args], cwd=cwd)
 
 
+def invoke_git(*args, cwd=None):
+    subprocess.check_call(["git", *args], cwd=cwd)
+
+
 class CMakeBuild(build_ext):
     def run(self):
         for ext in self.extensions:
@@ -39,17 +43,19 @@ class CMakeBuild(build_ext):
                 + ", ".join(e.name for e in self.extensions)
             )
 
-        mlir_dir = os.environ.get("WATER_MLIR_DIR", None)
-        if not mlir_dir:
-            raise RuntimeError(
-                "Expected MLIR directory to be provided via the WATER_MLIR_DIR environment variable"
-            )
-        if not os.path.isdir(mlir_dir) or not os.path.exists(
-            os.path.join(mlir_dir, "MLIRConfig.cmake")
-        ):
-            raise RuntimeError(
-                f"WATER_MLIR_DIR={mlir_dir} does not point to the MLIR cmake configuration"
-            )
+        built_monolithic = bool(int(os.environ.get("WATER_BUILD_MONOLITHIC", 0)))
+        if not built_monolithic:
+            mlir_dir = Path(os.environ.get("WATER_MLIR_DIR", None))
+            if not mlir_dir:
+                raise RuntimeError(
+                    "Expected MLIR directory to be provided via the WATER_MLIR_DIR environment variable"
+                )
+            if not os.path.isdir(mlir_dir) or not os.path.exists(
+                os.path.join(mlir_dir, "MLIRConfig.cmake")
+            ):
+                raise RuntimeError(
+                    f"WATER_MLIR_DIR={mlir_dir} does not point to the MLIR cmake configuration"
+                )
 
         build_type = os.environ.get("WATER_BUILD_TYPE", "Release")
 
@@ -62,16 +68,43 @@ class CMakeBuild(build_ext):
         ext_fullpath = Path.cwd() / self.get_ext_fullpath(ext.name)
         extdir = ext_fullpath.parent.resolve() / "water_mlir"
 
+        source_dir = Path(ext.sourcedir)
+        if built_monolithic:
+            llvm_dir = source_dir / "llvm-project"
+            llvm_sha = (source_dir / "llvm-sha.txt").read_text().strip()
+            if not os.path.exists(llvm_dir):
+                os.makedirs(llvm_dir, exist_ok=True)
+                invoke_git(
+                    "clone", "https://github.com/llvm/llvm-project.git", cwd=llvm_dir
+                )
+
+            invoke_git("fetch", cwd=llvm_dir)
+            invoke_git("checkout", llvm_sha, cwd=llvm_dir)
+            cmake_args = [
+                "-G Ninja",
+                f"-DLLVM_SRC_DIR={llvm_dir}/llvm",
+                "-DLLVM_TARGETS_TO_BUILD=host",
+                "-DLLVM_ENABLE_PROJECTS=mlir",
+                "-DLLVM_EXTERNAL_PROJECTS=water",
+                f"-DLLVM_EXTERNAL_WATER_SOURCE_DIR={source_dir}",
+                "-DBUILD_SHARED_LIBS=OFF",
+                "-DCMAKE_PLATFORM_NO_VERSIONED_SONAME=ON",
+                f"-DCMAKE_INSTALL_PREFIX={extdir}{os.sep}",
+                f"-DCMAKE_BUILD_TYPE={build_type}",
+            ]
+
+        else:
+            cmake_args = [
+                "-G Ninja",
+                f"-DMLIR_DIR={mlir_dir}",
+                "-DBUILD_SHARED_LIBS=OFF",
+                "-DCMAKE_PLATFORM_NO_VERSIONED_SONAME=ON",
+                f"-DCMAKE_INSTALL_PREFIX={extdir}{os.sep}",
+                f"-DCMAKE_BUILD_TYPE={build_type}",
+            ]
+
         # Configure CMake
-        cmake_args = [
-            "-G Ninja",
-            "-DCMAKE_PLATFORM_NO_VERSIONED_SONAME=ON",
-            f"-DMLIR_DIR={mlir_dir}",
-            "-DBUILD_SHARED_LIBS=OFF",
-            f"-DCMAKE_INSTALL_PREFIX={extdir}{os.sep}",
-            f"-DCMAKE_BUILD_TYPE={build_type}",
-        ]
-        invoke_cmake(ext.sourcedir, *cmake_args, cwd=build_dir)
+        invoke_cmake(source_dir, *cmake_args, cwd=build_dir)
 
         if not self.dry_run:
             # Build CMake project
